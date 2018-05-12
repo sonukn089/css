55799b36c35f8bcbeb8a522d44d687fa00e50274
Sonu <sonukn089@gmail.com>
2018-05-13 00:38:38 +0530

    push css page

--- css.php
+++ css.php
@@ -7,11 +7,17 @@
 			color: red;
 			text-align: center;
 		}
+		#para1 {
+			text-align: center;
+			color: blue;
+		}
 	</style>
 </head>
 <body>
 	<p>Hello World!</p>
-	<p>These paragraphs are styled with CSS.</p>
+	<p>These paragraphs are styled with CSS.</p><br>
+	<p id="para1">Hello World!</p>
+	<p>This paragraph is not affected by the style.</p>	
 
 </body>
 </html>
